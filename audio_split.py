f542d554aa5181c165efde292a77756c735a8c69
xgs <1720224284@qq.com>
2020-11-15 00:11:25 +0800

    小修小补

--- audio_split.py
+++ audio_split.py
@@ -2,34 +2,33 @@ from pydub import AudioSegment
 from pydub.utils import mediainfo
 
 # 请先使用朗读女将rpy_make生成的合总txt转换成mp3，并生成lrc文件
-sourcePath = '输出/潮土油P1/audio'
-destPath = '输出/潮土油P1/audio'
+sourcePath = '输出/潮土油P3/audio'
+destPath = '输出/潮土油P3/audio'
 
 #获取时间分割位点
-f_lyr =  open(sourcePath+'/log_o.lrc', mode = 'r', encoding='ANSI')
+f_lyr =  open(sourcePath+'/voice.lrc', mode = 'r', encoding='ANSI')
 lyr = f_lyr.readlines()
 time_split = [0]
-for i in range(1,len(lyr)):
-    if lyr[i][-2:-1]=='|':
+for i in range(1,len(lyr)-1):
+    if lyr[i][-2:-1]!='|':
         line = lyr[i+1]
         tmp = len(line.split(']')[0])-9
         min = line[1:tmp+3]
         s = line[tmp+4:tmp+6]
         ls = line[tmp+7:tmp+9]
         # 单位是ms
-        # 朗读女提供的歌词文件时间分割位点不准，这里进行了修偏，经过实验，对2h以内的音频都是有效的
-        time_stamp = 10*(int(ls) + 100*int(s) + 60*100*int(min)) - int(len(time_split) * 0.6)
+        # 朗读女提供的歌词文件时间分割位点不准
+        time_stamp = 10*int(ls) + 1000*int(s) + 60*1000*int(min)
         time_split.append(time_stamp)
 
-print(mediainfo(sourcePath+'\log.mp3')['duration'])
+print(mediainfo(sourcePath+'/voice.mp3')['duration'])
 # 读取mp3，切割输出
-mp3 = AudioSegment.from_mp3(sourcePath+'\log.mp3')
+mp3 = AudioSegment.from_mp3(sourcePath+'/voice.mp3')
 for i in range(len(time_split)-1):
 # for i in range(10):
     print(i)
     time_begin = time_split[i]
     time_over = time_split[i+1]
-    # # 不知为何朗读女合成大段语音时会有电流麦，为规避此现象，请尽量避免使用微软的几位发音人
     # 已解决，是分句空读所致
     mp3[time_begin:time_over].export(destPath+'/'+str(i)+'.mp3',format='mp3')
 
--- main.py
+++ main.py
@@ -1,104 +1,111 @@
 import os
-from aip import AipSpeech
 
 ##----此处填写内容开始----
 
 # 填入log路径和目标路径
-sourcePath = "源文本/潮土油P0"
-destPath = "输出/潮土油P0"
-# 人物名称列表，为了便于处理，请将骰娘也加入其中
-roles = ['墨刺','木刀','村上','镇长','路人甲','店长','瓦拉','小满']
+sourcePath = "源文本/潮土油P3"
+destPath = "输出/潮土油P3"
+#是否要生成rpy文本 1.是 2.否
+rpy_mode = 1
+# 声音合成模式 0.不合成 1.合总合成 2.分段合成
+audio_mode = 2
 # 骰娘列表
 diceGirls = ['小满']
-roles_param = [
-    {'原名':'墨刺','显示名':'墨刺'},
-    {'原名':'木刀','显示名':'木刀'},
-    {'原名':'村上','显示名':'村上'},
-    {'原名':'镇长','显示名':'镇长'},
-    {'原名':'路人甲','显示名':'路人甲'},
-    {'原名':'店长','显示名':'店长'},
-    {'原名':'瓦拉','显示名':'瓦拉'},
-    {'原名': '小满', '显示名': '小满'},
-]
+
+##----此处填写内容结束----
 
 f_log =  open(sourcePath+'/log_o.txt', mode = 'r', encoding='utf-8')
 if not os.path.exists(destPath+"/audio"): os.makedirs(destPath+"/audio")
 
-##----此处填写内容结束----
-
-#台词顺序-角色表
+roles = []
 orders = []
+dialogues = []
 
 #分段读取log，按人物切割
 log_paras = f_log.readlines()
 for i in range(len(log_paras)):
     try:
-        if (log_paras[i].split('>')[1] == '\n'):
-            print('出现空行，空行的前一行内容是' + log_paras[i - 1])
-    except IndexError:
-        print(i)
-    name = log_paras[i][1:].split('>')[0]
-    for j in range(len(roles)):
-        if(roles[j]==name):
-            orders.append(j)
-            break
-        if(j==len(roles)-1):
-            orders.append('error')
+        if(log_paras[i][0]=='#'):
+            continue
+        name = log_paras[i][1:].split('>')[0]
+        if (name not in roles):
+            roles.append(name)
+        orders.append(roles.index(name))
+        dialogues.append(''.join(log_paras[i].split('>')[1:])[1:-1])
+    except Error:
+        print('format wrong in txt line',i+1)
 
-if_rpy = int(input('是否要生成rpy文本\n1.是\n2.否\n'))
-if(if_rpy == 1):
+dialogues_size = len(dialogues)
+print(roles)
+
+if(rpy_mode == 1):
     f_rpy = open(destPath + '/script.rpy', mode='w', encoding='utf-8')
     # 头文件请自行定义
     f_rpy.write('label start:\n')
-    # f_rpy.write('    scene ' + bg_param['背景名'].split('.')[0] + '\n')
     # 内容部分
     voice_num = 0
-    for i in range(len(orders)):
+    for i in range(dialogues_size):
         # 开头
+        if (orders[i]=='error'):
+            print('error in',i)
+            continue
         if (i == 0):
             f_rpy.write(
-                '    show ' + roles_param[orders[i]]['显示名'] + '\n')
+                '    show ' + roles[orders[i]] + '\n')
         # 需要更换立绘
         if (i != 0 and orders[i] != orders[i - 1]):
-            f_rpy.write('    hide ' + roles_param[orders[i - 1]]['显示名'] + '\n\n')
-            f_rpy.write('    show ' + roles_param[orders[i]]['显示名'] + '\n')
-        if (orders[i] < len(roles) - len(diceGirls)):
+            f_rpy.write('    hide ' + roles[orders[i - 1]] + '\n\n')
+            f_rpy.write('    show ' + roles[orders[i]] + '\n')
+        if (roles[orders[i]] not in diceGirls):
             f_rpy.write('    voice \'../audio/' + str(voice_num) + '.mp3\'\n')
             voice_num += 1
         else:# 骰娘音效
             f_rpy.write('    voice \'../audio/骰娘的声音.mp3\'\n')
-        f_rpy.write('    ' + roles_param[orders[i]]['显示名'] + ' \'' + log_paras[i][1:].split('>')[1][1:-1] + '\'\n')
+        f_rpy.write('    ' + roles[orders[i]]+ ' \'' + dialogues[i] + '\''+'\n')
 
     f_rpy.write('\n    return')
 
-if_audio = int(input('是否要生成声音合成文本\n1.是\n2.否\n'))
+bad = ['。','，','？','！','.','（','）','/','"'] # 连续则停用字符
+meme = ['，','。','.','...','......','？？？','？','！','！？','？！'] # 防止被误杀
 
-bad = ['。','，','？','！','.','（','）','/','"'] # 停用词
+def getWord(s):
+    word = ''
+    j = 0
+    if(len(s)==1):
+        return s
+    while (j < len(s)):
+        while (j < len(s) - 1 and dialogues[i][j] in bad and s[j + 1] in bad):
+            j += 1
+        if (s[j] in bad):
+            if (0 < j < len(s) - 1):
+                word += '|'
+        else:
+            word += s[j]
+        j += 1
+        # 去掉引起电流麦的空读
+    return word
 
-if(if_audio==1):
-    audio_mode = int(input('请选择声音合成模式\n1.合总合成\n2.分段合成\n'))
-    if (audio_mode == 1):
-        # 朗读女txt，合总
-        f_voice = open(destPath + '/audio/log_o.txt', mode='w', encoding='utf-8')
-        # f_voice.write('<\_@TTS\_/>\n')
-        # for j in range(len(roles_voice_param)-len(diceGirls)):
-        #     f_voice.write('<_/1>'+roles_voice_param[j]['原名']+'<_/2>'+roles_voice_param[j]['发音人']+'<_/3>'+str(roles_voice_param[j]['音量'])+'<_/4>'+str(roles_voice_param[j]['语速'])+'<_/5>'+str(roles_voice_param[j]['语调'])+'<_/6></++/>\n')
-        # f_voice.write('<\_@TTS\_/>')
-        for i in range(len(log_paras)):
-            if (orders[i] < len(roles) - len(diceGirls)):
-                while(log_paras[i][-2:-1] in bad):
-                    log_paras[i] = log_paras[i][:-2] + '\n' # 去掉引起电流麦的空读
-                f_voice.write('[' + log_paras[i].split('>')[0][1:] + ']' + log_paras[i].split('>')[1][1:-1] + '|\n')  #为了使歌词顺利分隔，此处加入无实际意义的|
-    elif (audio_mode == 2):
-        # 朗读女txt，分段
-        voice_num = 0
-        for i in range(len(log_paras)):
-            if (orders[i] < len(roles) - len(diceGirls)):
-                if not os.path.exists(destPath + '/audio/' + str(orders[i])): os.makedirs(destPath + '/audio/' + str(orders[i]))
-                f_voice = open(destPath + '/audio/' + str(orders[i]) + '/' + str(voice_num) + '.txt', mode='w',encoding='utf-8')
-                f_voice.write(log_paras[i][1:].split('>')[1][1:])
-                voice_num += 1
-    else:
-        print('wrong input')
+if (audio_mode == 1):
+    # 朗读女txt，合总
+    f_voice = open(destPath + '/audio/voice.txt', mode='w', encoding='utf-8')
+    for i in range(dialogues_size):
+        if (roles[orders[i]] not in diceGirls):
+            word = dialogues[i]
+            if (word not in meme):
+                word = getWord(word)
+            f_voice.write('[' + roles[orders[i]] + ']' + ' ' + word + '\n')
+
+elif (audio_mode == 2):
+    # 朗读女txt，分段
+    voice_num = 0
+    for i in range(dialogues_size):
+        if (roles[orders[i]] not in diceGirls):
+            audio_file_name = roles[orders[i]]
+            if not os.path.exists(destPath + '/audio/' + audio_file_name): os.makedirs(
+                destPath + '/audio/' + audio_file_name)
+            f_voice = open(destPath + '/audio/' + audio_file_name + '/' + str(voice_num) + '.txt', mode='w',
+                           encoding='utf-8')
+            f_voice.write(dialogues[i]+'\n')
+            voice_num += 1
 
 print("over")
